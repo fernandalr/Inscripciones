448fa48b842485bf5591cab72fd2998df1ced508
Fernanda <spmferch_0205@outlook.com>
2025-03-28 09:55:50 -0600

    Primer commit

---
+++ src/main/java/mx/uam/integracion/Inscripciones/InscripcionesApplication.java
@@ -0,0 +1,13 @@
+package mx.uam.integracion.Inscripciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InscripcionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InscripcionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/uam/integracion/Inscripciones/InscripcionesApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uam.integracion.Inscripciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InscripcionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
